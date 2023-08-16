adb509ea7d27abf3c0833ee3c052a82945514f9b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-16 14:08:15 -0400

    create nearby_markets method using sql haversine formula

--- app/models/market.rb
+++ app/models/market.rb
@@ -12,42 +12,13 @@ class Market < ActiveRecord::Base
   end
 
   def self.nearby_markets(location_params)    
-    require 'pry'; binding.pry
-    where(haversine(location_params, {latitude:, longitude:}) <= location_params[:radius])
+    where("acos(
+      sin(radians(#{location_params[:latitude]})) 
+        * sin(radians(latitude)) 
+      + cos(radians(#{location_params[:latitude]})) 
+        * cos(radians(latitude)) 
+        * cos( radians(#{location_params[:longitude]})
+          - radians(longitude))
+      ) * 3958.8 <= #{location_params[:radius]}")
   end
-
-  def market_coordinates
-    {latitude: latitude, longitude: longitude}
-  end
-
-  def self.haversine(start_coords, end_coords)
-    #The haversine formula is as follows: 
-    #a = sin²(φB — φA/2) + cos φA * cos φB * sin²(λB — λA/2)
-    # c = 2 * atan2( √a, √(1−a) )
-    # d = R ⋅ c
-
-    #radius of the earth in miles
-    earths_radius_mi = 3958.8
-
-    # latitude degrees become radians
-    start_latitude = radians(start_coords[:latitude])
-    end_latitude = radians(end_coords[:latitude])
-  
-
-    latitude_difference = radians(end_coords[:latitude] - start_coords[:latitude])
-    longitude_difference = radians(end_coords[:longitude] - start_coords[:longitude])
-  
-    # sin²(φB - φA/2) + cos φA * cos φB * sin²(λB - λA/2)
-    a = Math.sin(latitude_difference/2.0)**2 + Math.cos(start_latitude) * Math.cos(end_latitude) * Math.sin(longitude_difference/2.0)**2
-  
-    # c = 2 * atan2( √a, √(1−a) )
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-  
-    #distance between degrees
-    earths_radius_mi * c
-  end
-
-  def self.radians(degrees)
-    degrees * (Math::PI / 180)
-  end  
 end
