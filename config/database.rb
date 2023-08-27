854ae838802bccd1bb8b8c41064eb227640028bd
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-27 12:14:53 -0400

    reconfigure database connection

--- config/database.rb
+++ config/database.rb
@@ -3,14 +3,26 @@ database_name = "microservice_sinatra_#{Microservice.environment}"
 
 db = URI.parse( ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 
+configure :development do
+
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  # :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
-  :host => db.host,
-  :port => db.port,
-  :username => db.user,
-  :password => db.password,
+  adapter: db.scheme == "postgres" ? "postgresql" : db.scheme,
   database: "#{database_name}",
   encoding: "utf8"
 )
+end
+
+configure :production do
+  # connect ActiveRecord with the current database
+  ActiveRecord::Base.establish_connection(
+    # adapter: 'postgresql',
+    adapter: db.scheme == "postgres" ? "postgresql" : db.scheme,
+    host: db.host,
+    port: db.port,
+    username: db.user,
+    password: db.password,
+    database: "#{database_name}",
+    encoding: "utf8"
+  )
+end
