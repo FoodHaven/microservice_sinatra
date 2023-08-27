7e862409ddaf8b67a2dc240b580ba3af4df7001d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-27 11:48:38 -0400

    configure database

--- config/database.rb
+++ config/database.rb
@@ -7,10 +7,10 @@ db = URI.parse( ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   # :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
-  # :host => db.host,
-  # :port => db.port,
-  # :username => db.user,
-  # :password => db.password,
+  :host => db.host,
+  :port => db.port,
+  :username => db.user,
+  :password => db.password,
   database: "#{database_name}",
   encoding: "utf8"
 )
