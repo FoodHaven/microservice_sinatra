8e2f0b059d7c55eb7542f68e1f0f684f6e6dcc85
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-26 20:37:15 -0400

    change database file to be more detailed

--- config/database.rb
+++ config/database.rb
@@ -9,8 +9,8 @@ ActiveRecord::Base.establish_connection(
   :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
   :host => db.host,
   :port => db.port,
-  # :username => db.user,
-  # :password => db.password,
+  :username => db.user,
+  :password => db.password,
   database: "#{database_name}",
   encoding: "utf8"
 )
