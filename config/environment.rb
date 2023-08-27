ca18687ad5e46e249c2d39be1836a738a53fb36f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-26 21:04:02 -0400

    help

--- config/environment.rb
+++ config/environment.rb
@@ -14,5 +14,5 @@ require File.join(APP_ROOT, 'config', 'database')
 class Microservice < Sinatra::Base
   set :method_override, true
   set :root, APP_ROOT
-  # set :database_file, "./database.yml"
+  set :database_file, "./database.yml"
 end
