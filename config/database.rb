9f00eaa546badcc15819abc384ee531675f2a6b3
Jesse Sorman <jessesorman@gmail.com>
2024-12-05 14:10:31 -0500

    add aws database env

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 # set the database based on the current environment
 database_name = "microservice_sinatra_#{Microservice.environment}"
 
-db = URI.parse( ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+db = URI.parse( ENV['RDS_HOSTNAME'] || "postgres://localhost/#{database_name}")
 
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
